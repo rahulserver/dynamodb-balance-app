6285367b02e152e9a7ae7f00df9e4b7be784581a
rahulserver <rahulserver@gmail.com>
2024-12-31 22:59:37 +0530

    add proper error messages to validate transact function

--- lib/functions/transact.ts
+++ lib/functions/transact.ts
@@ -6,15 +6,20 @@ export const transact = async (input: {
 }): Promise<void> => {
   const { idempotentKey, userId, amount, type } = input;
 
-  if (
-    !idempotentKey ||
-    !userId ||
-    amount <= 0 ||
-    !['credit', 'debit'].includes(type)
-  ) {
-    throw new Error(
-      'Invalid input: All fields are required and must be valid.',
-    );
+  if (!idempotentKey) {
+    throw new Error('Idempotent key is required');
+  }
+
+  if (!userId) {
+    throw new Error('User ID is required');
+  }
+
+  if (amount <= 0) {
+    throw new Error('Amount must be greater than 0');
+  }
+
+  if (!['credit', 'debit'].includes(type)) {
+    throw new Error('Transaction type must be either "credit" or "debit"');
   }
 
   // Placeholder for logic
